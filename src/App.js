f9656027d22e1974d85fa4ef4153439a73fefde1
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-09-22 21:53:34 +0900

    useClick

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useRef } from "react";
+
+const useClick = (onClick) => {
+  const element = useRef();
+
+  useEffect(() => {
+    if (typeof onClick !== "function") return;
+
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        console.log("unmount");
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+
+  return element;
+};
 
 function App() {
+  const log = () => {
+    console.log("HELLO!");
+  };
+  const title = useClick(log);
+
   return (
     <div className="App">
       <h1>Hello</h1>
+      <button ref={title}>ASD</button>
     </div>
   );
 }
