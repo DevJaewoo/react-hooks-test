ba93fd9a3b61608f37f70a19a87d58d6cf7165e5
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-09-22 21:26:17 +0900

    useTitle

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [number, setNumber] = useState(0);
-  const [aNumber, setAnumber] = useState(0);
-
-  const sayHello = () => console.log("Hello!");
-  useEffect(sayHello, [number]);
-
   return (
     <div className="App">
       <h1>Hello</h1>
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
     </div>
   );
 }
