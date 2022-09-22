b277e27a5151d969b08aab19657b37b83a487e9e
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-09-22 21:13:49 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useTabs } from "./useTabs";
-
-const content = [
-  {
-    tab: "Section 1",
-    content: "I'm the content of the Section 1",
-  },
-  {
-    tab: "Section 2",
-    content: "I'm the content of the Section 2",
-  },
-];
+import { useEffect, useState } from "react";
 
 function App() {
-  const { currentItem, changeItem } = useTabs(0, content);
+  const [number, setNumber] = useState(0);
+  const [aNumber, setAnumber] = useState(0);
+
+  const sayHello = () => console.log("Hello!");
+  useEffect(sayHello, [number]);
 
   return (
     <div className="App">
       <h1>Hello</h1>
-      {content.map((section, index) => (
-        <button onClick={() => changeItem(index)}>{section.tab}</button>
-      ))}
-      <p>{currentItem.content}</p>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
     </div>
   );
 }
