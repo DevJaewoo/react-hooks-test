a9bcb2f09c3229f005f69ad70707977ac764fbf7
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-09-24 04:32:17 +0900

    useFadeIn

---
+++ src/useFadeIn.js
@@ -0,0 +1,13 @@
+const useFadeIn = (duration = 1, delay = 0) => {
+  const element = useRef();
+  useEffect(() => {
+    if (typeof duration !== "number" || typeof delay !== "number") return;
+    const { current } = element;
+    current.style.transition = `opacity ${duration}s ease-in-out`;
+    current.style.transitionDelay = `${delay}s`;
+    current.style.opacity = 1;
+  }, []);
+  return { ref: element, style: { opacity: 0 } };
+};
+
+export { useFadeIn };
