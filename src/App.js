53ef6c9de2bc7ebc911aa30ca686b33f14740eb9
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-09-23 21:06:54 +0900

    useNetwork

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import "./App.css";
 import { useRef, useState, useEffect } from "react";
 
-const useFadeIn = (duration = 1, delay = 0) => {
-  const element = useRef();
+//callback을 받아 네트워크가 변경됐을 때 callback 호출, state 변경
+const useNetwork = (callback) => {
+  const [status, setStatus] = useState(navigator.onLine);
+
+  const handler = () => {
+    if (typeof callback === "function") {
+      callback(navigator.onLine);
+    }
+    setStatus(navigator.onLine);
+  };
+
   useEffect(() => {
-    if (typeof duration !== "number" || typeof delay !== "number") return;
-    const { current } = element;
-    current.style.transition = `opacity ${duration}s ease-in-out`;
-    current.style.transitionDelay = `${delay}s`;
-    current.style.opacity = 1;
+    window.addEventListener("online", handler);
+    window.addEventListener("offline", handler);
+    return () => {
+      window.removeEventListener("offline", handler);
+      window.removeEventListener("online", handler);
+    };
   }, []);
-  return { ref: element, style: { opacity: 0 } };
+
+  return status;
 };
 
 function App() {
+  const status = useNetwork((value) => {
+    console.log(`network changed: ${value}`);
+  });
+
   return (
     <div className="App">
-      <h1 {...useFadeIn(2, 2)}>Hello</h1>
-      <p {...useFadeIn(10)}>asdasd</p>
+      <h1>Hello</h1>
+      <p>{status ? "Online" : "Offline"}</p>
     </div>
   );
 }
